c05256c0a4ccfef42fb57c37bb8c9a01dfaf2193
Yong Guan <gyong9@gmail.com>
2013-11-11 22:46:00 -0800

    change it to in-browser app

--- app/background.js
+++
@@ -1,7 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function () {
-//    chrome.tabs.create({url: 'index.html'});
-//    chrome.app.window.open('index.html');
-    chrome.app.window.create('index.html', {
-        'state': 'maximized'
-    });
-});
