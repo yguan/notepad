6dc5b0afa4432476ae84596c424a5dcfe77c0857
Yong Guan <gyong9@gmail.com>
2013-11-13 18:51:50 -0800

    make the widget minimum size smaller

--- app/js/view/notes.js
+++ app/js/view/notes.js
@@ -15,15 +15,15 @@ define(function (require, exports, module) {
     exports.controller = function ($scope, $location, $document, $timeout, $modal, $sce) {
         var gridsterSize = gridsterSizeCalculator.getSizeInfo({
             container: '.notes',
-            widgetMinWidth: 300,
-            widgetMinHeight: 250,
+            widgetMinWidth: 100,
+            widgetMinHeight: 100,
             padding: 5
         });
 
         $scope.notes = [];
 
         function getDefaultGridsterItemOptions() {
-            return {row: 1, col: 1, size_x: 1, size_y: 1};
+            return {row: 1, col: 1, size_x: 3, size_y: 2};
         }
 
         function getDefaultNote() {
